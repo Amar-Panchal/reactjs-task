a8600af67ecd8a78c6a75346aee7c8224bced249
Amar Panchal <91047824+Amar-Panchal@users.noreply.github.com>
2022-01-01 14:11:10 +0530

    Update Poster.js

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -4,6 +4,8 @@ import {FaArrowLeft} from 'react-icons/fa'
 import {IoCloseSharp} from 'react-icons/io5'
 import {BsFacebook} from 'react-icons/bs'
 import {FcGoogle} from 'react-icons/fc'
+import signup from '../Images/signup.jpg'
+
 function Poster() {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
@@ -11,27 +13,29 @@ function Poster() {
     const [signInshow, setsignInShow] = useState(false);
     const signInhandleClose = () => setsignInShow(false);
     const signInhandleShow = () => setsignInShow(true);
+
+    
     return (
         <>
         <div className="poster">
-            <div className="mobile-poster mt-4 mx-4 d-flex justify-content-between  d-lg-none">
+            <div className="mobile-poster mt-4 mx-4 d-flex d-lg-none justify-content-between  ">
                 <h4><FaArrowLeft/></h4>
-                <button className='join-btn' onClick={handleShow}>Join Group</button>
+                <button className='join-btn' onClick={signInhandleShow}>Join Group</button>
             </div>
-            <Modal show={show}  size="lg"  onHide={handleClose} className="mobile-modalbody  modalbody d-sm-block d-md-block d-lg-none" >
+            <Modal show={show}  size="xl" aria-labelledby="contained-modal-title-vcenter" centered onHide={handleClose} className="mobile-modalbody mt-4 modalbody mx-0" >
                    
                
                 <Modal.Body >
-                    <div className=" sign-up d-flex   justify-content-between align-items-center ">
+                    <div className=" sign-up d-flex pt-3  justify-content-between align-items-center ">
                         <h5>Create Account</h5>
                         <button className="close" onClick={handleClose}>
                          <IoCloseSharp />
                         </button>
                         
                     </div>
-                    <div className="signup-form d-flex justify-content-between align-content-center">
+                    <div className="signup-form mobile-signup-form d-flex justify-content-between align-content-center">
                         <div>
-                            <div className=" d-inline-flex">   
+                            <div className=" d-flex">   
                                 <input className="name" type="text" placeholder="First Name" />
                                 <input className="name name2" type="text" placeholder="Last Name" />
                             </div>
@@ -40,21 +44,56 @@ function Poster() {
                                 <input type="password" placeholder="Password" /><br />
                                 <input type="text" placeholder="Confirm Password" />
                             </div>
-                            <div className="d-flex justify-content-between  align-items-center">
+                            <div className="mobile-signup-link  justify-content-between  align-items-center py-3">
                             <button className="mobile-signup-btn">Create Account</button>
-                            <p ><span onClick={signInhandleShow}> or,Sign In </span></p>
+                            <p  onClick={signInhandleShow}> or,Sign In </p>
                             </div>
                             <button className='facebooksingup-btn'><span><BsFacebook /></span> Sign up with Facebook</button>
                             <button className='googlesingup-btn'><span><FcGoogle /></span> Sign up with Google</button>
-                            <p className="mobile-terms text-center mx-4">By signing up, you agree to our Terms & conditions, Privacy policy</p>
+                            <p className="mobile-terms text-center mt-3">By signing up, you agree to our Terms & conditions, Privacy policy</p>
 
+                        </div>
+                        <div>
+                            <img src={signup} alt="" />
                         </div>
                     </div>
                 </Modal.Body>
                 
                 </Modal>
 
-
+                <Modal show={signInshow}  size="xl" aria-labelledby="contained-modal-title-vcenter" centered onHide={signInhandleClose} className="mobile-modalbody mt-0 modalbody mx-0" >
+                   
+               
+                   <Modal.Body >
+                       <div className=" sign-up d-flex pt-4 pb-4  justify-content-between align-items-center ">
+                           <h5>Welcome back</h5>
+                           <button className="close" onClick={signInhandleClose}>
+                            <IoCloseSharp />
+                           </button>
+                           
+                       </div>
+                       <div className="signup-form mobile-signup-form d-flex justify-content-between align-content-center">
+                           <div>
+                               <div>
+                                   <input type="email" placeholder="Email" /><br/>
+                                   <input type="password" placeholder="Password" /><br />
+                               </div>
+                               <div className="mobile-signup-link justify-content-between  align-items-center pt-5 pb-4" >
+                               <button className="mobile-signup-btn">Sign In</button>
+                               <p  onClick={handleShow}> or ,  Create Account </p>
+                               </div>
+                               <button className='facebooksingup-btn mb-3'><span><BsFacebook /></span> Sign up with Facebook</button>
+                               <button className='googlesingup-btn'><span><FcGoogle /></span> Sign up with Google</button>
+                               <p className="mobile-terms text-center mt-3 pb-5">Forgot Password</p>
+   
+                           </div>
+                           <div>
+                               <img src={signup} alt="" />
+                           </div>
+                       </div>
+                   </Modal.Body>
+                   
+                   </Modal>
                
             <h2>Computer Engineering</h2>
             <p>142,765 Computer Engineers follow this</p>
